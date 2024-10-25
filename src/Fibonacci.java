4f44e43625cc768aabfbc1b272fff6b64dc9e06c
gusta-santos-dev <gusta.ribsantos@gmail.com>
2024-10-25 15:54:43 -0300

    initial commit

---
+++ src/Fibonacci.java
@@ -0,0 +1,30 @@
+import java.util.Scanner;
+
+public class Fibonacci {
+    public static void main(String[] args) {
+        Scanner scanner = new Scanner(System.in);
+
+        // Usuario informa um numero
+        System.out.print("Informe um número: ");
+        int numero = scanner.nextInt();
+        
+
+        // Variáveis iniciais da sequência de Fibonacci
+        int Num1 = 0, Num2 = 1;
+        int fibonacci = 0;
+
+        // Calcula a sequência de Fibonacci até que o valor seja maior ou igual ao número informado
+        while (fibonacci < numero) {
+            fibonacci = Num1 + Num2;
+            Num1 = Num2;
+            Num2 = fibonacci;
+        }
+
+        // Verifica se o número pertence à sequência de Fibonacci
+        if (fibonacci == numero || numero == 0) {
+            System.out.println("O número " + numero + " pertence à sequência de Fibonacci.");
+        } else {
+            System.out.println("O número " + numero + " não pertence à sequência de Fibonacci.");
+        }
+    }
+}
